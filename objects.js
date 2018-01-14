ee6842eb8d7c43d8473c35b6047b92d35b16f607
Vika Suhorukova <viktorya.sukhorukova@gmail.com>
2018-01-14 01:09:55 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = new Object(['madonna','dream']);
 
 function updatePlaylist(obj, artistName, songTitle) {
-  obj.artistName = songTitle;
+  obj[artistName] = songTitle;
   return obj;
 }
